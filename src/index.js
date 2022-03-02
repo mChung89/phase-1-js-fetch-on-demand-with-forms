d94368f2114c0a6468842912994474a88c9b1d7d
Michael Chung <mikechung89@gmail.com>
2022-03-02 00:38:17 -0500

    commit tree e1907a1a7fa5e910c6ca56bf63c80529c3436dcc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = e.target.children[1].value
+        fetch(`http://localhost:3000/movies/${input}`).then(resp => resp.json()).then(json => {
+            const title = document.querySelector('#movieDetails h4')
+            title.textContent = json.title
+            const summary = document.querySelector(`#movieDetails p`)
+            summary.textContent = json.summary
+        })
+        inputForm.reset()
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
